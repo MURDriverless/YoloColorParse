d5a518e5b937be3e55e6893d981d5eca7a106267
Andrew Huang <huanga2@student.unimelb.edu.au>
2020-05-26 17:36:47 +1000

    working poc UI

--- main.m
+++ main.m
@@ -2,29 +2,43 @@ clc;
 clear;
 close all;
 
+global mainFigure clickColorID boxColorsStr boxColors boxDatas frameID objectIds
+
+frameID = 1;
+
 boxDatas = readtable('yolov3-training_all.csv', 'HeaderLines', 2);
-boxDatas = sortrows(boxDatas, 'Var1', 'descend');
 
-boxColors = {'blue', '#ffa500', 'yellow'};
+boxColors = {'blue', [255,165,0]/255, 'yellow'};
+boxColorsStr = {'Blue', 'Orange', 'Yellow'};
 objectIds = [0, 1, 2];
-
-drawImages = 0;
 
 mkdir('./output')
 
-figure(1)
-for ii = 1
+mainFigure = figure('units','normalized');
+clickColorID = 0;
+hold on
+disableDefaultInteractivity(gca)
+set(mainFigure,'KeyPressFcn',@hotKeyFunc);
+updateClickColor();
+
+loadImage(frameID);
+
+return
+
+function loadImage(ii)
+    global boxDatas rectangs fileName objectIds boxColorsStr boxColors
     fileName = boxDatas(ii,1);
     boundStrings = boxDatas(ii, 6:end);
     
     frame = imread(['./YOLO_Dataset/', fileName.(1){1}]);
-    outputFile = fopen(['./output/', fileName.(1){1}(1:end-3), 'txt'], 'w');
     
-    if drawImages
-        figure(1)
-        imshow(frame);
-        hold on
-    end
+    cla;
+    imshow(frame, 'parent', gca);
+    title(fileName.(1){1}, 'Interpreter', 'none');
+    xlim([1 size(frame,2)]);
+    ylim([1 size(frame,1)]);
+    
+    rectangs = {};
 
     for jj = 1:numel(boundStrings)
         boundString = boundStrings.(jj){1}; % X0, Y0, H0, W0
@@ -37,54 +51,98 @@ for ii = 1
         
         crop = frame(boundBox(2) + (1:boundBox(3)), ...
                      boundBox(1) + (1:boundBox(4)), :);
-                 
-%         figure
-%         imshow(crop);
                   
 %         crop = crop(:, (-1:1) + floor(size(crop,2)/2), :);
 %         crop = crop(floor(size(crop,1)/2):end, :, :);
         
-        crop_HSV = rgb2hsv(crop);
+%         crop_HSV = rgb2hsv(crop);
         
         colorCount = [0, 0, 0];
         
         % Blue
-%         channel1Min = 0.533;
-%         channel1Max = 0.804;
-%         colorCount(1) = sum((channel1Min <= crop_HSV(:,:,1)) & (crop_HSV(:,:,1) <= channel1Max), 'all');
         [mask, ~] = createMaskBlue(crop); % RGB masking
         colorCount(1) = sum(mask, 'all');
         
         % Orange
-%         channel1Min = 0.016;
-%         channel1Max = 0.134;
-%         colorCount(3) = sum((channel1Min <= crop_HSV(:,:,1)) & (crop_HSV(:,:,1) <= channel1Max), 'all');
         [mask, ~] = createMaskOrange(crop);
         colorCount(2) = sum(mask, 'all');
         
         % Yellow
-%         channel1Min = 0.128;
-%         channel1Max = 0.181;
-%         colorCount(2) = sum((channel1Min <= crop_HSV(:,:,1)) & (crop_HSV(:,:,1) <= channel1Max), 'all');
         [mask, ~] = createMaskYellow(crop);
         colorCount(3) = sum(mask, 'all');
 
         coneColorID = objectIds(colorCount == max(colorCount));
         coneColor = boxColors(colorCount == max(colorCount));
         
-%         figure(ehh)
-        if drawImages
-            rectangle('Position', boundBox([1 2 4 3]), 'EdgeColor', coneColor{1});
-        end
-%         images.roi.Rectangle(gca, 'Position', boundBox([1 2 4 3]), 'Color', coneColor{1}, 'InteractionsAllowed', 'none');
+        rectangs{jj} = images.roi.Rectangle(gca, 'Position', boundBox([1 2 4 3]), 'Color', coneColor{1}, 'InteractionsAllowed', 'translate');
+        addlistener(rectangs{jj},'ROIClicked',@allevents);
 
         Xc = (boundBox(1) + boundBox(4)/2)/size(frame, 2);
         Yc = (boundBox(2) + boundBox(3)/2)/size(frame, 1);
         H = (boundBox(3))/size(frame, 1);
         W = (boundBox(4))/size(frame, 2);
         
-        fprintf(outputFile, '%d %0.6f %0.6f %0.6f %0.6f\n', coneColorID, Xc, Yc, W, H);
+        rectangs{jj}.UserData = {coneColorID, Xc, Yc, H, W};
+    end
+end
+
+function allevents(src,evt)
+    global clickColorID boxColors
+    evname = evt.EventName;
+    switch(evname)
+        case{'ROIClicked'}
+            src.UserData{1} = clickColorID;
+            src.Color = boxColors{clickColorID+1};
+    end
+end
+
+function saveOutputFile()
+    global rectangs fileName
+    temp = fileName.(1){1};
+    
+    outputFile = fopen(['./output/', temp(1:end-3), 'txt'], 'w');
+    
+    for ii = 1:numel(rectangs)
+        boundData = rectangs{ii}.UserData;
+        fprintf(outputFile, '%d %0.6f %0.6f %0.6f %0.6f\n', boundData{1}, ... 
+                                                            boundData{2}, ... 
+                                                            boundData{3}, ... 
+                                                            boundData{4}, ...
+                                                            boundData{5});
     end
     
     fclose(outputFile);
+end
+
+function hotKeyFunc(src,event)
+    global clickColorID frameID
+    switch(event.Key)
+        case('1')
+            clickColorID = 0;
+            updateClickColor();
+        case('2')
+            clickColorID = 1;
+            updateClickColor();
+        case('3')
+            clickColorID = 2;
+            updateClickColor();
+%         case('q')
+%             saveOutputFile()
+%             
+%             if frameID > 1
+%                 frameID = frameID - 1;
+%                 loadImage(frameID);
+%             end
+        case('e')
+            saveOutputFile()
+            frameID = frameID + 1;
+            loadImage(frameID);
+    end
+    
+end
+
+function updateClickColor()
+    global mainFigure boxColorsStr clickColorID
+    figure(mainFigure);
+    xlabel(boxColorsStr{clickColorID+1});
 end
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 Put ``YOLO_Dataset``and ``yolov3-training_all.csv`` in root folder
 
 Outputs to ``output/``
+
+## Manual Usage
+Press 1, 2, 3 for Blue, orange, yellow.
+
+Left click bounding box to change color.
+
+Press E to advance frame.
+
+### SCRIPT CURRENTLY REWRITES OUTPUT TEXTFILE, TEXTFILE PREVIEW TBD
