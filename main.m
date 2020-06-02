99d3dd7bf2799db120793e6b5755fd9b7e40ead5
Steven Lee <stevenlee090@gmail.com>
2020-06-02 17:14:57 +1000

    Manual UI for Label Review (#2)
    * working poc UI
    
    * updated readme
    
    * more updates to readme
    
    * Sort video frames in ascending order
    
    * updated readme to include download and label review instructions
    
    * updated readme to include direct download link for dataset
    
    * updated readme
    
    * Changed from ROI.rect to basic.Rect, added magnification
    
    * fixed some typos and updated readme for new features
    
    * fix for multiple color detected
    
    * added frameID to plot title
    
    * updated readme
    
    Co-authored-by: Andrew Huang <huanga2@student.unimelb.edu.au>

--- main.m
+++ main.m
@@ -2,32 +2,61 @@ clc;
 clear;
 close all;
 
+global mainFigure clickColorID boxColorsStr boxColors boxDatas frameID objectIds startingFrameID
+
+startingFrameID = 1;
+
+outputFolderName = './output';
+
+%% Do not touch
+frameID = startingFrameID;
 boxDatas = readtable('yolov3-training_all.csv', 'HeaderLines', 2);
+
+% sort video frames in ascending order to ensure consistent ordering
 boxDatas = sortrows(boxDatas, 'Var1', 'ascend');
 
-boxColors = {'blue', '#ffa500', 'yellow'};
+boxColors = {'blue', [255,165,0]/255, 'yellow'};
+boxColorsStr = {'Blue', 'Orange', 'Yellow'};
 objectIds = [0, 1, 2];
 
-drawImages = 1;
+if ~exist(outputFolderName, 'dir')
+    % Folder does not exist so create it.
+    mkdir(outputFolderName);
+end
 
-mkdir('./output')
+mainFigure = figure('units','normalized');
+clickColorID = 0;
+hold on
+disableDefaultInteractivity(gca)
+set(mainFigure,'KeyPressFcn',@hotKeyFunc);
+updateClickColor();
 
-figure(1)
-for ii = 200:400
+loadImage(frameID);
+
+return
+
+function loadImage(ii)
+    global boxDatas rectangs fileName objectIds boxColorsStr boxColors frame
     fileName = boxDatas(ii,1);
     boundStrings = boxDatas(ii, 6:end);
     
     frame = imread(['./YOLO_Dataset/', fileName.(1){1}]);
-    outputFile = fopen(['./output/', fileName.(1){1}(1:end-3), 'txt'], 'w');
     
-    if drawImages
-        figure(1)
-        imshow(frame);
-        hold on
-    end
+    cla;
+    dispImage = imshow(frame, 'parent', gca);
+    set(dispImage, 'ButtonDownFcn', @axisEvents);
+    title([fileName.(1){1}, ', frameID: ', num2str(ii)], 'Interpreter', 'none');
+    xlim([1 size(frame,2)]);
+    ylim([1 size(frame,1)]);
+    
+    rectangs = {};
 
     for jj = 1:numel(boundStrings)
         boundString = boundStrings.(jj){1}; % X0, Y0, H0, W0
+        
+        % before extracting bounding box locations, remove any double
+        % quotes inside boundString, otherwise str2num may return an error
+        boundString = strrep(boundString, '"', '');
         
         if numel(boundString) < 1
             break;
@@ -41,55 +70,128 @@ for ii = 200:400
         
         crop = frame(boundBox(2) + (1:boundBox(3)), ...
                      boundBox(1) + (1:boundBox(4)), :);
-                 
-%         figure
-%         imshow(crop);
                   
 %         crop = crop(:, (-1:1) + floor(size(crop,2)/2), :);
 %         crop = crop(floor(size(crop,1)/2):end, :, :);
         
-        crop_HSV = rgb2hsv(crop);
+%         crop_HSV = rgb2hsv(crop);
         
         colorCount = [0, 0, 0];
         
         % Blue
-%         channel1Min = 0.533;
-%         channel1Max = 0.804;
-%         colorCount(1) = sum((channel1Min <= crop_HSV(:,:,1)) & (crop_HSV(:,:,1) <= channel1Max), 'all');
         [mask, ~] = createMaskBlue(crop); % RGB masking
         colorCount(1) = sum(mask, 'all');
         
         % Orange
-%         channel1Min = 0.016;
-%         channel1Max = 0.134;
-%         colorCount(3) = sum((channel1Min <= crop_HSV(:,:,1)) & (crop_HSV(:,:,1) <= channel1Max), 'all');
         [mask, ~] = createMaskOrange(crop);
         colorCount(2) = sum(mask, 'all');
         
         % Yellow
-%         channel1Min = 0.128;
-%         channel1Max = 0.181;
-%         colorCount(2) = sum((channel1Min <= crop_HSV(:,:,1)) & (crop_HSV(:,:,1) <= channel1Max), 'all');
         [mask, ~] = createMaskYellow(crop);
         colorCount(3) = sum(mask, 'all');
 
         coneColorID = objectIds(colorCount == max(colorCount));
         coneColor = boxColors(colorCount == max(colorCount));
         
-%         figure(ehh)
-        if drawImages
-            rectangle('Position', boundBox([1 2 4 3]), 'EdgeColor', coneColor{1});
-%             pause(0.1)
-        end
-%         images.roi.Rectangle(gca, 'Position', boundBox([1 2 4 3]), 'Color', coneColor{1}, 'InteractionsAllowed', 'none');
+        % Event that no color is detect or multiple colors is detected,
+        % take first
+        coneColorID = coneColorID(1);
+        coneColor = coneColor(1);
+
+        rectangs{jj} = rectangle('Position', boundBox([1 2 4 3]), ...
+        'linewidth', 1, ...
+        'EdgeColor', coneColor{1}, ...
+        'FaceColor', [0, 0, 0, 0.01], ...
+        'ButtonDownFcn', @rectEvents);
 
         Xc = (boundBox(1) + boundBox(4)/2)/size(frame, 2);
         Yc = (boundBox(2) + boundBox(3)/2)/size(frame, 1);
         H = (boundBox(3))/size(frame, 1);
         W = (boundBox(4))/size(frame, 2);
         
-        fprintf(outputFile, '%d %0.6f %0.6f %0.6f %0.6f\n', coneColorID, Xc, Yc, W, H);
+        rectangs{jj}.UserData = {coneColorID, Xc, Yc, H, W};
+    end
+end
+
+function axisEvents(src,evt)
+    evname = evt.EventName;
+    selectType = src.Parent.Parent.SelectionType;
+    
+    if strcmp(evname, 'Hit') && strcmp(selectType, 'alt')
+        xlim([1 size(src.CData,2)]);
+        ylim([1 size(src.CData,1)]);
+    end
+end
+
+function rectEvents(src,evt)
+    global clickColorID boxColors frame
+    evname = evt.EventName;
+    
+    % Parent is axis, Parent.Parent is figure
+    selectType = src.Parent.Parent.SelectionType;
+    if strcmp(evname, 'Hit')
+        if strcmp(selectType, 'normal')
+            src.UserData{1} = clickColorID;
+            src.EdgeColor = boxColors{clickColorID+1};
+        elseif strcmp(selectType, 'alt')
+            boxDim = [src.UserData{2:5}];
+            
+            xlim(size(frame,2) * (boxDim(1) + boxDim(4) .* [-1, 1]));
+            ylim(size(frame,1) * (boxDim(2) + boxDim(3) .* [-1, 1]));
+        end
+    end
+end
+
+function saveOutputFile()
+    global rectangs fileName
+    temp = fileName.(1){1};
+    
+    outputFile = fopen(['./output/', temp(1:end-3), 'txt'], 'w');
+    
+    for ii = 1:numel(rectangs)
+        boundData = rectangs{ii}.UserData;
+        fprintf(outputFile, '%d %0.6f %0.6f %0.6f %0.6f\n', boundData{1}, ... 
+                                                            boundData{2}, ... 
+                                                            boundData{3}, ... 
+                                                            boundData{4}, ...
+                                                            boundData{5});
     end
     
     fclose(outputFile);
+end
+
+function hotKeyFunc(src,event)
+    global clickColorID frameID startingFrameID frame
+    switch(event.Key)
+        case('1')
+            clickColorID = 0;
+            updateClickColor();
+        case('2')
+            clickColorID = 1;
+            updateClickColor();
+        case('3')
+            clickColorID = 2;
+            updateClickColor();
+        case('q')
+            saveOutputFile()
+            
+            if frameID > startingFrameID
+                frameID = frameID - 1;
+                loadImage(frameID);
+            end
+        case('e')
+            saveOutputFile()
+            frameID = frameID + 1;
+            loadImage(frameID);
+        case('r')
+            xlim([1 size(frame,2)])
+            ylim([1 size(frame,1)])
+    end
+    
+end
+
+function updateClickColor()
+    global mainFigure boxColorsStr clickColorID
+    figure(mainFigure);
+    xlabel(boxColorsStr{clickColorID+1});
 end
