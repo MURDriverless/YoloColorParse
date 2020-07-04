acd18e97b6b4c4a48c4eb8073d6e56e94272c130
Steven Lee <stevenlee090@hotmail.com>
2020-07-04 16:04:38 +1000

    updated text preview example to support start and end ID

--- PreviewExample.m
+++ PreviewExample.m
@@ -2,8 +2,17 @@ clc;
 clear;
 close all;
 
-frameSet = FrameSet('./YOLO_Dataset', './output');
+startID = 1;
+endID = 500;
 
-dinfo = dir('./output/*.txt');
-tmp = cellfun(@(x) x(1:end-4), {dinfo.name}, 'UniformOutput', 0);
+% give directory to image dataset and frames labels
+frameSet = FrameSet('./YOLO_Dataset', '../YoloColorParse_Data/frames');
+
+boxDatas = readtable('yolov3-training_all.csv', 'HeaderLines', 2);
+boxDatas = sortrows(boxDatas, 'Var1', 'ascend');
+
+frameNames = boxDatas.(1)(startID:endID);
+dinfo = cellfun(@(x) x, frameNames, 'UniformOutput', 0)';
+
+tmp = cellfun(@(x) x(1:end-4), dinfo, 'UniformOutput', 0);
 frameSet.setPreviewList(tmp);
